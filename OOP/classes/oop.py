a2900f48f89fa8ec7be66eb3fd8f4bcd910b2573
Miguel Vasquez <miguelvasq3z@gmail.com>
2024-08-27 21:41:41 -0500

    Initializing repository

---
+++ OOP/classes/oop.py
@@ -0,0 +1,3 @@
+#Object oriented programming is one of the most effective approaches to writting software. I object oriented programming you write classes
+#that represent real world things and situations, and you create objects based on these classes.
+
